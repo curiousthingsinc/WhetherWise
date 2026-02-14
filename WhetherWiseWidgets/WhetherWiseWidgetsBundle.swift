5a31879735554b9448e917ec3b033f9e88b61a7d
Eric Lobdell <eric@curiousthings.co>
2026-02-14 12:46:17 -0500

    added widgets target

---
+++ WhetherWiseWidgets/WhetherWiseWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WhetherWiseWidgetsBundle.swift
+//  WhetherWiseWidgets
+//
+//  Created by Eric Lobdell on 2/14/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WhetherWiseWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        WhetherWiseWidgets()
+    }
+}
