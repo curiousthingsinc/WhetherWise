a8dd9ac88375c3f4750fbd2321699fe150cacf54
Eric Lobdell <eric@curiousthings.co>
2026-02-14 12:07:38 -0500

    Initial Commit

---
+++ WhetherWiseTests/WhetherWiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhetherWiseTests.swift
+//  WhetherWiseTests
+//
+//  Created by Eric Lobdell on 2/14/26.
+//
+
+import Testing
+@testable import WhetherWise
+
+struct WhetherWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
