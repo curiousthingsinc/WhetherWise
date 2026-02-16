d0e397e397b93586341d08b171efc6fd9145aa1e
Eric Lobdell <eric@curiousthings.co>
2026-02-16 13:22:35 -0500

    Have weather unit settings working

---
+++ WhetherWiseCore/Sources/WhetherWiseCore/Views/WeatherSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  WhetherWiseCore
+//
+//  Created by Eric Lobdell on 2/16/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
